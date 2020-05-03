5a8297ab155ea058cf7b0b8b7fa71471d5f54f16
Bhavuk Girdhar <bhavuk.girdhar@soprasteria.com>
2020-05-03 16:43:27 +0530

    CreateArtifact.sh committed

--- CreateArtifact.sh
+++ CreateArtifact.sh
@@ -1,1 +1,1 @@
-mvn deploy:deploy-file -Dfile=${WORKSPACE}\target\access-database-0.0.1-SNAPSHOT.jar -DgroupId=com.bhavuk.database -DartifactId=access-database  -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar  -Durl=file:./local-maven-repository/ -DrepositoryId=maven-repository  -DupdateReleaseInfo=true
+mvn deploy:deploy-file -Dfile=${WORKSPACE}/target/access-database-0.0.1-SNAPSHOT.jar -DgroupId=com.bhavuk.database -DartifactId=access-database  -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar  -Durl=file:./local-maven-repository/ -DrepositoryId=maven-repository  -DupdateReleaseInfo=true
