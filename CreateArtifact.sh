202b9020ae321ed4a122086d7a8ea3aa26623d0c
Bhavuk Girdhar <bhavuk.girdhar@soprasteria.com>
2020-05-03 17:41:58 +0530

    code added

--- CreateArtifact.sh
+++ CreateArtifact.sh
@@ -1,1 +1,5 @@
 mvn deploy:deploy-file -Dfile=${WORKSPACE}/target/access-database-0.0.1-SNAPSHOT.jar -DgroupId=com.bhavuk.database -DartifactId=access-database  -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar  -Durl=file:./local-maven-repository/ -DrepositoryId=maven-repository  -DupdateReleaseInfo=true
+cd /
+sudo mkdir -p maven-repository
+sudo chown -R jenkins:jenkins maven-repository
+cp ${WORKSPACE}/local-maven-repository/. -av /maven-repository
